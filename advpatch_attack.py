e247d76575fd478240d3a2f73a0296f4e19bd5ec
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2025-02-23 20:12:56 +0800

    refectoring

--- advpatch_attack.py
+++ advpatch_attack.py
@@ -1,122 +1,138 @@
+import json
 import torch
 from ultralytics import YOLO
-from advattack.attack import PatchAttacker
+from advattack.attacker import PatchAttacker
 from advattack.train import AdvPatchAttack_YOLODetector_Trainer
 from advattack.validation import PatchAttack_DetValidator
 from pathlib import Path
-from typing import Literal
 from argparse import ArgumentParser
-from tools import write_yaml, write_json
+from tools import write_json, args2dict, load_yaml
+from ultralytics.utils.torch_utils import select_device, init_seeds
+from advattack.attacker import DEFAULT_ATTACKER_CFG_FILE
+from advattack.inference import compare_visualization
+
 
 def validataion(
-    pretrained_weight:Path, patch_path:Path, 
-    dataset_cfg:Path, proj:Path, seed:int, 
-    device:torch.device=torch.device("cuda:0"), batchsz:int=16,
-    random_rotate_patch=False, debug=False, **kwargs
+    data:Path, detector:Path, pretrained_patch:Path, 
+    project:Path, name:str,   
+    batch:int=16,  attacker=DEFAULT_ATTACKER_CFG_FILE,
+    seed:int=89112, deterministic:bool=True, device:str='0',
+    patch_random_rotate:bool=False, patch_blur:bool=False,
+    debug:bool=False, 
+    **kwargs
 ):
-    proj.mkdir(parents=True, exist_ok=True)
-    model = YOLO(pretrained_weight)
+    
+    init_seeds(seed=seed, deterministic=deterministic)
+    device = select_device(device=device, verbose=False)
+
+    (project/name).mkdir(parents=True, exist_ok=True)
+    model = YOLO(detector)
     model = model.to(device=device)
-    print(f"attacking with {patch_path}")
-    patch = torch.load(patch_path, map_location='cpu')['patch']
+    print(f"attacking with {pretrained_patch}")
+    patch = torch.load(pretrained_patch, map_location=model.device)['patch']
     validator = PatchAttack_DetValidator.build_according_YOLO_with_dataset(
-        model=model, dataset_cfg=dataset_cfg, 
-        batchsz=batchsz, save_dir=proj, attacker=PatchAttacker()
+        model=model, data=data, 
+        batch=batch, save_dir=project/name, 
+        attacker=PatchAttacker(**load_yaml(attacker))
     )
     metrics = validator.comparsion(
         model=model.model,
         adv_patch=patch, 
-        random_rotate_patch=random_rotate_patch,
-        plot_attacked_img=debug
+        patch_random_rotate=patch_random_rotate,
+        patch_blur=patch_blur,
+        debug=debug
     )
-    print(metrics)
-    write_json(
-        {'data':str(dataset_cfg), 'seed':seed, 'metrics':metrics},
-        proj/"val_metrics.json"
+    print(json.dumps(metrics,indent=4))
+    write_json({'data':str(data), 'seed':seed, 'metrics':metrics},
+        project/name/"val_metrics.json"
     )
 
-def train_advpatch(
-    pretrained_weight:Path, dataset_cfg:Path, project:Path, seed:int,  
-    device:str='0',
-    method:Literal['gd', 'pdg']='gd', lr:float=2e-2,
-    epochs:int=1000, patience:int=None, 
-    batchsz:int=16, psize:int=300, 
-    scale_det_loss:bool=False, 
-    random_rotate_patch:bool=False,
-    debug:bool=False, **kwargs
-):
+def train_advpatch(trainer_args, patch_transform_args, hyp, **kwargs):
+    advpatch_trainer = AdvPatchAttack_YOLODetector_Trainer(**trainer_args)
+    train_result = advpatch_trainer.train(**hyp, preprocess_args=patch_transform_args)
+    print(json.dumps(train_result, indent=4))
 
-    advpatch_trainer = AdvPatchAttack_YOLODetector_Trainer(
-        pretrained_weight=pretrained_weight, 
-        dataset_cfg=dataset_cfg,
-        seed=seed,
-        device=device,
-        save_dir=project, 
-        scale_det_loss=scale_det_loss,
-        batchsz=batchsz, psize=psize
-    )
-    write_yaml(
-        o={
-            'data':str(dataset_cfg),
-            'pretrained_weight':str(pretrained_weight),
-            'lr':lr,
-            'epochs':epochs,
-            "batchsz":batchsz,
-            'psize':psize,
-            "det_loss_scale":scale_det_loss,
-            "random_rotate_patch":random_rotate_patch,
-            'seed':seed,
-            'patience':patience
-        }, 
-        f=project/"args.yaml"
-    )
-    train_result = advpatch_trainer.train(
-        method=method, 
-        lr=lr,
-        epoch=epochs,
-        patience=patience,
-        preprocess_args={
-            'random_rotate_patch':random_rotate_patch, 
-            'debug':debug
-        }
-    )
-    print(train_result)
-
-
-def main():
-    pass
-
-if __name__ == "__main__":
+def lazy_arg_parsers():
     
     parser = ArgumentParser()
-    
-    parser.add_argument("--task", type=str, default='train')
-    parser.add_argument("--seed", type=int, default=891122)
-    parser.add_argument("--project", type=Path)
-    parser.add_argument("--method", type=str, default='gd')
-    parser.add_argument("--lr", type=float, default=2e-2)
-    parser.add_argument("--epochs", type=int, default=500)
-    parser.add_argument("--patience", type=int, default=100)
-    parser.add_argument("--pretrained_weight", type=Path, default=Path("pretrained")/"yolov8n.pt")
+
+    parser.add_argument("--task", type=str, choices=['train','val', 'infer'], default='train')   
     parser.add_argument("--patch_path", type=Path)
-    parser.add_argument("--dataset_cfg", type=Path, default=Path("INRIAPerson")/"inria.yaml")
-    parser.add_argument("--batchsz", type=int, default=16)
-    parser.add_argument("--det_loss_scale", action='store_true')
+
+    # patch transform
+
+    cfg_keys = {
+        "patch_transform_args":["patch_random_rotate", "patch_blur"],
+        "trainer_args":[
+            "detector", "data",
+            "project", "name", "psize", "ptype", "attacker", "batch", "device",
+            "seed", "deterministic", "tensorboard"
+        ],
+        "validator_args":[
+            "detector", "pretrained_patch", 
+            "data","project",
+            "name", "seed", "deterministic", "device"
+        ],
+        "hyp":["lr", "epochs", "patience"]
+    }
+    
+    
+    parser.add_argument("--detector", type=Path, default=Path("pretrained")/"yolov8n.pt")
+    parser.add_argument("--data", type=Path, default=Path("INRIAPerson")/"inria.yaml")
+    parser.add_argument("--project", type=Path, default=Path("run"))
+    parser.add_argument("--name", type=str, default="advp_attack")
     parser.add_argument("--psize", type=int, default=300)
-    parser.add_argument('--no_tensorboard', action='store_true')
-    parser.add_argument("--not_rotate", action='store_true')
-    parser.add_argument("--debug", action='store_true')
+    parser.add_argument("--ptype", type=str,choices=['random', 'gray'], default='random')
+    parser.add_argument("--attacker", type=Path, default=DEFAULT_ATTACKER_CFG_FILE)
+    parser.add_argument("--batch", type=int, default=16)
     parser.add_argument("--device", type=str, default='0')
+    parser.add_argument("--seed", type=int, default=891122)
+    parser.add_argument("--not_deterministic", action='store_true')
+    parser.add_argument('--no_tensorboard', action='store_true')
+    parser.add_argument("--pretrained_patch", type=Path)
+    parser.add_argument("--patch_random_rotate", action='store_true')
+    parser.add_argument("--patch_blur", action='store_true')
+    
+    # training hyp
+    
+    parser.add_argument("--lr", type=float, default=1e-2)
+    parser.add_argument("--epochs", type=int, default=50)
+    parser.add_argument("--patience", type=int, default=10)
+    parser.add_argument("--det_loss_scale", action='store_true')
+    parser.add_argument("--method", type=str, default='gd')
+    
+    parser.add_argument("--debug", action='store_true')
+    
+    cli_args = args2dict(parser.parse_args())
+    task = cli_args['task']
+    args = {k: {j:cli_args[j] for j in v} for k,v in cfg_keys.items() }
+    
+    if task in ['val', 'infer']:
+        # TODO: refactoring validator so that it can be like 
+        # trainer directly takes hierarchical dict
+        args = {**(args['validator_args']), **(args['patch_transform_args'])}
+        if args['pretrained_patch'] is None:
+            potential_patch_path = args['project']/args['name']/f"worst.pt"
+            if not potential_patch_path.is_file():
+                potential_patch_path = args['project']/args['name']/f"last.pt"
+            
+            assert potential_patch_path.is_file()
+            print(f"using {potential_patch_path} according `project` and `name` arguments to choose pretrained patch")
+            args['pretrained_patch'] = potential_patch_path
+    
+    return task, args
 
-    cli_args = vars(parser.parse_args()) 
-    cli_args["tensorboard"] = not cli_args["no_tensorboard"]
-    cli_args["random_rotate_patch"] = not cli_args["not_rotate"]
 
-    match cli_args["task"]:
-        case 'train':
-            train_advpatch(**cli_args)
-        case 'val':
-            pass
-            validataion(**cli_args)
-        case _:
-            raise KeyError(f"No such a task {cli_args['task']}.")
+task_map = {
+    'train':train_advpatch,
+    'val':validataion,
+    'infer':compare_visualization
+}
+
+def main():
+    task, args = lazy_arg_parsers()
+    task_map[task](**args)
+
+if __name__ == "__main__":
+    main()
+    
