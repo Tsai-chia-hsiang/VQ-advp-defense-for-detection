abce66865177b9071ebe29a0e98c57e75196c67d
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2025-02-23 20:29:30 +0800

    customize args tools

--- tools/cfg_utils.py
+++ tools/cfg_utils.py
@@ -1,5 +1,6 @@
 import yaml
 import json
+from argparse import Namespace
 
 def load_yaml(yf):
     with open(yf, "r") as fp:
@@ -18,4 +19,29 @@ def read_json(f, encoding='utf-8'):
 def write_json(o, f):
     with open(f, "w+") as jf:
         json.dump(o, jf, indent=4, ensure_ascii=False)
+
+def args2dict(args:Namespace|dict, wanted_keys:list[str]=None, neg_prefix:tuple[str]=('no_', 'not_')) -> dict:
+    
+    def to_postive(k:str, v:bool)->tuple[str, bool]:
+        for n in neg_prefix:
+            if k.startswith(n):
+                return k.replace(n, ''), not v
+        return k, v
+    
+    value = vars(args) if not isinstance(args, dict) else args
+    processed_value = {}
+
+    if wanted_keys is None:
+        wanted_keys = value
+    
+    for k in wanted_keys:
+        pk = k
+        v = value[k]
+        if isinstance(value[k], bool):
+            pk, v = to_postive(k=pk, v=v)
+        
+        processed_value[pk] = v
+
+    return processed_value
+
 
