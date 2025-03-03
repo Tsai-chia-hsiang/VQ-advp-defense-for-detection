434fb9cc1d99ab70e738be2bdac7d50ba44603c3
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2025-03-03 21:42:00 +0800

    add diferent color for different class of detected objects

--- advattack/ultralytics_utils.py
+++ advattack/ultralytics_utils.py
@@ -5,6 +5,7 @@ import cv2
 import torch
 import torch.nn as nn
 from ultralytics.utils import DEFAULT_CFG
+from ultralytics.utils.plotting import colors
 from ultralytics.data.utils import check_det_dataset
 from ultralytics.utils.checks import check_imgsz
 from ultralytics.utils import IterableSimpleNamespace
@@ -84,18 +85,46 @@ def ultralytics_yolobatch_draw_boxes(batch, save_to:Path=None, return_img_lst:bo
     if save_to is not None:
         save_to.mkdir(parents=True, exist_ok=True)
     
+    sfunc = (lambda x:x*255) if need_scale else None
+    
     for i, timg in enumerate(batch['img']):
         idx_mask:torch.Tensor = torch.where(batch['batch_idx'] == i)[0]
+        its_cls:torch.Tensor = batch['cls'][idx_mask]
         its_boxes:torch.Tensor = batch['bboxes'][idx_mask]
-        img = tensor2img(timg, scale_back_f=lambda x:x*255 if need_scale else lambda x:x)
+        
+        img = tensor2img(timg, scale_back_f=sfunc)
         xyxy = xywh2xyxy(xywh=its_boxes.cpu().detach().numpy())
-        draw_boxes(img=img, xyxy=scale_box(xyxy, imgsize=np.asarray(img.shape[:2][::-1]), direction='back'))
+        xyxy=scale_box(xyxy, imgsize=np.asarray(img.shape[:2][::-1]), direction='back')
+        draw_boxes(img=img, xyxy=xyxy, color=[colors(int(j)) for j in its_cls])
         if save_to is not None:
             cv2.imwrite(save_to/Path(batch['im_file'][i]).name, img=img)
         if return_img_lst:
             ret.append(img)
     
+    gc.collect()
     if return_img_lst:
+        return ret    
+
+def ultralytics_yolobatch_det_draw_boxes(batch, preds:list[np.ndarray|torch.Tensor], labels:list[str], wanted_cls:list[int]=None, save_to:Path=None, return_img:bool=False, need_scale=False) -> list[np.ndarray]|None:
+
+    ret = []
+
+    if save_to is not None:
+        save_to.mkdir(parents=True, exist_ok=True)
+
+    for timg, name, boxes in zip(batch['img'], batch['im_file'], preds):
+        img = tensor2img(timg, scale_back_f=lambda x:x*255 if need_scale else lambda x:x)
+        b = boxes if wanted_cls is None else boxes[np.isin(boxes[:, -1].astype(np.int32), wanted_cls), :]
+        if len(b) > 0:
+            c, l = zip(*((colors(c), f"{labels[c]}{conf:.2f}") for c, conf in zip(b[:, -1], b[:, 4])))
+            
+            draw_boxes(img=img, xyxy=b[:, :4], color=c, label=l)
+        if return_img:
+            ret.append(img)
+        if save_to is not None:
+            cv2.imwrite(save_to/Path(name).name, img=img)
+    
+    gc.collect()
+    if return_img:
         return ret
-    else:
-        gc.collect()
+        
