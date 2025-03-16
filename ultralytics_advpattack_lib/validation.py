24d81061f9ea272e6730ce01c1894ea483d7e3aa
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2025-03-17 05:39:16 +0800

    refactoring to easy init

--- ultralytics_advpattack_lib/validation.py
+++ ultralytics_advpattack_lib/validation.py
@@ -2,32 +2,88 @@ from pathlib import Path
 from typing import Optional, Any
 from tqdm import tqdm
 import torch
+import numpy as np
+import cv2
+from ultralytics.data.build import InfiniteDataLoader
 from ultralytics import YOLO
 import torch.nn as nn
 from ultralytics.nn.tasks import DetectionModel
 from ultralytics.models.yolo.detect.val import DetectionValidator
-from ultralytics.data.build import build_yolo_dataset, build_dataloader
-from ultralytics.utils.torch_utils import de_parallel, smart_inference_mode
-from .attacker import PatchAttacker
-from .ultralytics_utils import get_data_args
+from ultralytics.utils.torch_utils import de_parallel
+from .attacker import PatchAttacker, DEFAULT_ATTACKER_CFG_FILE
+from .ultralytics_utils import get_data_args, get_dataloader
 import sys
 import os
 from pathlib import Path
 sys.path.append(os.path.abspath(Path(__file__).parent))
+from torchcvext import tensor2img
+from tools import load_yaml
 from taming_transformers.ultray_do import ultralytics_batch_recons
 
-class PatchAttack_DetValidator(DetectionValidator):
+
+class AdvPatchAttack_YOLODetector_Validator(DetectionValidator):
     
-    def __init__(self, attacker:PatchAttacker, conf:float=0.25, dataloader=None, save_dir=None, pbar=None, args=None, _callbacks=None):
-        super().__init__(dataloader, save_dir, pbar, args, _callbacks)
-        self.attacker = attacker
+    def __init__(
+        self, detector:Path|YOLO, save_dir:Path,
+        data:Optional[Path]=None, loader:Optional[InfiniteDataLoader]=None,
+        conf:float=0.25,
+        attacker:Path|PatchAttacker=DEFAULT_ATTACKER_CFG_FILE, 
+        imgsz:int=512, batchsz:int=16, 
+        pbar=None, _callbacks=None, **kwargs
+    ):
+        assert any([data, loader])
+        if loader is None:
+            assert detector is not None, "no loader, need `data` to build loader"
+        
+        reference_model=YOLO(detector) if isinstance(detector, Path) else detector
+        
+        loader = AdvPatchAttack_YOLODetector_Validator.get_dataloader_according_YOLO(
+            reference_model,
+            dataset_cfgfile_path=data, 
+            imgsz=imgsz, batch=batchsz
+        ) if loader is None else loader
+ 
+        args = {
+            **(reference_model.overrides),
+            **{'mode':'val', 'imgsz':loader.dataset.imgsz,'rect':loader.dataset.rect, 'batch':loader.batch_size}
+        }
+        save_dir.mkdir(parents=True, exist_ok=True)
+        super().__init__(dataloader=loader, save_dir=save_dir, pbar=pbar, args=args, _callbacks=_callbacks)
+        self.args.plots = False
+        self.data = self.dataloader.dataset.data
+        self.attacker = PatchAttacker(**load_yaml(attacker)) if isinstance(attacker, Path) else attacker
         self.training = False
         self.args.conf = conf
+    
+    @staticmethod
+    def get_dataloader_according_YOLO(reference_model:YOLO, dataset_cfgfile_path:Path, imgsz:int=512, batch:int=16)->InfiniteDataLoader:
+        data_args, dataset_args = get_data_args(
+            model_args={**(reference_model.overrides),**{'imgsz':imgsz}}, 
+            dataset_cfgfile_path=dataset_cfgfile_path,
+            stride=max(int(reference_model.model.stride.max()), 32),
+            batch=batch,
+        )
+        print(dataset_args)
+        data_args.workers = 1
+        assert 'val' in dataset_args
 
+        return get_dataloader(data_args=data_args, dataset_args=dataset_args, split='val')
+  
     def init_metrics(self, model:nn.Module):
         self.device = next(model.parameters()).device
         return super().init_metrics(model)
     
+    def _save_img(self, batch)->None:
+        imgs = tensor2img(batch["img"])
+        array = batch["img"].detach().permute(0, 2, 3, 1).cpu().numpy()
+        if not (self.save_dir/"img").is_dir():
+            (self.save_dir/"img").mkdir(parents=True, exist_ok=True)
+        if not (self.save_dir/"np").is_dir():
+            (self.save_dir/"np").mkdir(parents=True, exist_ok=True)
+        for im, a, name in zip(imgs, array, batch['im_file']):
+            cv2.imwrite(self.save_dir/"img"/Path(name).name, im)
+            np.save(self.save_dir/"img"/Path(name).name)
+        
     def __call__(self, model:DetectionModel, adv_patch:Optional[torch.Tensor]=None, **pr_args):
                 
         self.init_metrics(de_parallel(model))
@@ -84,43 +140,3 @@ class PatchAttack_DetValidator(DetectionValidator):
             for k in clean_metrics
         }
 
-    @classmethod
-    def build_according_YOLO_with_dataset(cls, data:Path, attacker:PatchAttacker, model:YOLO=None, model_args:dict=None, conf:float=0.25, rect:bool=False, batch:int=16, save_dir:Optional[Path]=None)->"PatchAttack_DetValidator":
-        """
-        Need to pass at least 1 of `model`, `model_args`.
-        - If both are pass, using model.overrides as default 
-        """
-
-        data_args, dataset_args = get_data_args(
-            model_args=model.overrides if model is not None else model_args, 
-            stride=max(int(model.model.stride.max()), 32),
-            dataset_cfgfile_path = data,
-            batch=batch,
-            mode='val'
-        )
-        data_args.workers = 1
-        loader = build_dataloader(
-            dataset = build_yolo_dataset(
-                cfg = data_args,
-                img_path=dataset_args.get('val'),
-                batch=data_args.batch, 
-                data=dataset_args, 
-                mode=data_args.mode, 
-                stride = data_args.stride,
-                rect=rect
-            ),
-            batch=data_args.batch, shuffle=False, 
-            workers=data_args.workers
-        )
-
-        validator = cls( 
-            dataloader=loader,
-            conf=conf,
-            attacker=attacker,
-            save_dir=save_dir,
-            args=model.overrides|{'mode':'val','rect':rect, 'batch':batch, 'data':data},
-            _callbacks=model.callbacks
-        )
-        validator.data = dataset_args
-        validator.args.plots = False
-        return validator
