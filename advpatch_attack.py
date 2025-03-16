cc3732fe7ec88acaa9969d7e92d64fcbc416145d
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2025-03-17 05:44:12 +0800

    change the lib name

--- advpatch_attack.py
+++ advpatch_attack.py
@@ -1,52 +1,36 @@
 import json
 import torch
 from ultralytics import YOLO
-from advattack.attacker import PatchAttacker
-from advattack.train import AdvPatchAttack_YOLODetector_Trainer
-from advattack.validation import PatchAttack_DetValidator
+from ultralytics_advpattack_lib.train import AdvPatchAttack_YOLODetector_Trainer
+from ultralytics_advpattack_lib.validation import AdvPatchAttack_YOLODetector_Validator
 from pathlib import Path
 from argparse import ArgumentParser
-from tools import write_json, args2dict, load_yaml
+from tools import write_json, args2dict
 from ultralytics.utils.torch_utils import select_device, init_seeds
-from advattack.attacker import DEFAULT_ATTACKER_CFG_FILE
-from advattack.inference import compare_visualization
+from ultralytics_advpattack_lib.attacker import DEFAULT_ATTACKER_CFG_FILE
+from ultralytics_advpattack_lib.inference import compare_visualization
 
-
-def validataion(
-    data:Path, detector:Path, pretrained_patch:Path, 
-    project:Path, name:str,  conf:float=0.25, 
-    batch:int=16,  attacker=DEFAULT_ATTACKER_CFG_FILE,
-    seed:int=89112, deterministic:bool=True, device:str='0',
-    patch_random_rotate:bool=False, patch_blur:bool=False,
-    debug:bool=False, clean=False, vq:bool=False,
-    **kwargs
-):
+def validataion(validator_args, patch_transform_args, **kwargs):
     
-    init_seeds(seed=seed, deterministic=deterministic)
-    device = select_device(device=device, verbose=False)
-
-    (project/name).mkdir(parents=True, exist_ok=True)
-    model = YOLO(detector)
-    model = model.to(device=device)
-    print(f"attacking with {pretrained_patch}")
-    patch = torch.load(pretrained_patch, map_location=model.device)['patch']
-    validator = PatchAttack_DetValidator.build_according_YOLO_with_dataset(
-        model=model, data=data, 
-        batch=batch, save_dir=project/name, 
-        conf=conf,
-        attacker=PatchAttacker(**load_yaml(attacker))
-    )
+    init_seeds(validator_args['seed'], deterministic=True)
+    model = YOLO(validator_args['detector'])
+    model = model.to(device=select_device(f"{validator_args['device']}"))
+    patch = torch.load(validator_args['pretrained_patch'], map_location=model.device)['patch']
+    clean_only = validator_args['clean']
+    
+    del validator_args['detector'], validator_args['seed'], validator_args['pretrained_patch']
+    
+    validator = AdvPatchAttack_YOLODetector_Validator(detector=model, **validator_args)
+    
     metrics = validator.comparsion(
         model=model.model,
-        adv_patch=patch if not clean else None,
-        vq=vq, 
-        patch_random_rotate=patch_random_rotate,
-        patch_blur=patch_blur,
-        debug=debug
+        adv_patch=patch if not clean_only else None,
+        **patch_transform_args
     )
     print(json.dumps(metrics,indent=4))
-    write_json({'data':str(data), 'seed':seed, 'metrics':metrics},
-        project/name/"val_metrics.json"
+    write_json(
+        {'data':str(validator_args['data']), 'metrics':metrics}, 
+        validator.save_dir/"val_metrics.json"
     )
 
 def train_advpatch(trainer_args, patch_transform_args, hyp, **kwargs):
@@ -61,27 +45,26 @@ def lazy_arg_parsers():
     parser.add_argument("--task", type=str, choices=['train','val', 'infer'], default='train')   
     parser.add_argument("--patch_path", type=Path)
 
-    # patch transform
-
     cfg_keys = {
-        "patch_transform_args":["patch_random_rotate", "patch_blur"],
+        "patch_transform_args":["patch_random_rotate", "patch_blur", "vq"],
         "trainer_args":[
             "detector", "data","to_attack", "logit_to_prob", "conf",
-            "project", "name", "psize", "ptype", "attacker", "batch", "device",
-            "sup_prob_loss", 
+            "save_dir", "psize", "ptype", "attacker", "batch", "device",
+            "sup_prob_loss", "imgsz",
             "seed", "deterministic", "tensorboard"
         ],
         "validator_args":[
-            "detector", "pretrained_patch", 
-            "data","project", "attacker", "batch",
-            "name", "seed", "deterministic", "device",
-            "conf", "clean", "vq"
+            "detector", "pretrained_patch", "imgsz",
+            "data","save_dir", "attacker", "batch", 
+            "seed", "deterministic", "device",
+            "conf", "clean"
         ],
         "hyp":["lr", "epochs", "patience"]
     }
     
     
     parser.add_argument("--detector", type=Path, default=Path("pretrained")/"yolov8n.pt")
+    parser.add_argument("--imgsz", type=int, default=512)
     parser.add_argument("--sup_prob_loss", action='store_true')
     parser.add_argument("--logit_to_prob", action='store_true')
     parser.add_argument("--conf", type=float, default=0.5)
@@ -108,29 +91,31 @@ def lazy_arg_parsers():
     parser.add_argument("--lr", type=float, default=1e-2)
     parser.add_argument("--epochs", type=int, default=50)
     parser.add_argument("--patience", type=int, default=math.inf)
-    #parser.add_argument("--det_loss_scale", action='store_true')
     parser.add_argument("--method", type=str, default='gd')
-    # parser.add_argument("--det_terms", nargs='+', type=str, default=['box', 'prob'])
-    
+  
     parser.add_argument("--debug", action='store_true')
-    
-    cli_args = args2dict(parser.parse_args())
+    cli_args = parser.parse_args()
+    cli_args.save_dir = cli_args.project/cli_args.name
+    del cli_args.project, cli_args.name
+
+    if cli_args.pretrained_patch is None and cli_args.task != 'train':
+        potential_patch_path = cli_args.save_dir/f"worst.pt"
+        if not potential_patch_path.is_file():
+            potential_patch_path = cli_args.save_dir/f"last.pt"
+        assert potential_patch_path.is_file()
+        print(f"using {potential_patch_path} according `project` and `name` arguments to choose pretrained patch")
+        args.pretrained_patch = potential_patch_path
+
+
+    cli_args = args2dict(cli_args)
     task = cli_args['task']
     args = {k: {j:cli_args[j] for j in v} for k,v in cfg_keys.items() }
     
-    if task in ['val', 'infer']:
+    if task in ['infer']:
         # TODO: refactoring validator so that it can be like 
         # trainer directly takes hierarchical dict
         args = {**(args['validator_args']), **(args['patch_transform_args'])}
-        if args['pretrained_patch'] is None:
-            potential_patch_path = args['project']/args['name']/f"worst.pt"
-            if not potential_patch_path.is_file():
-                potential_patch_path = args['project']/args['name']/f"last.pt"
-            
-            assert potential_patch_path.is_file()
-            print(f"using {potential_patch_path} according `project` and `name` arguments to choose pretrained patch")
-            args['pretrained_patch'] = potential_patch_path
-    
+        
     return task, args
 
 
