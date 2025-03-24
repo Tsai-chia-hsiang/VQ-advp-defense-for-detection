e446e71f9feff8def23244c7f640c618468bbd21
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2025-03-24 10:16:52 +0800

    adding imgsz and chaning torchcvext to deepcvext

--- ultralytics_advpattack_lib/inference.py
+++ ultralytics_advpattack_lib/inference.py
@@ -1,5 +1,6 @@
 import torch
 import sys
+import cv2
 import numpy as np
 from tqdm import tqdm
 import os.path as osp
@@ -16,8 +17,16 @@ from ultralytics.utils.torch_utils import select_device, init_seeds
 from .import _LOCAL_DIR_
 sys.path.append(osp.abspath(_LOCAL_DIR_.parent))
 from tools import load_yaml, write_json
-from torchcvext.draw import canvas
-from torchcvext import tensor2img
+from deepcvext.draw import canvas
+from deepcvext import tensor2img
+
+def make_img_np_dir(root:Path)->tuple[Path, Path]:
+    arr = root/"arr"
+    arr.mkdir(parents=True,exist_ok=True)
+    img = root/"img"
+    img.mkdir(parents=True, exist_ok=True)
+    return (img, arr)
+
 
 def pred_once(model:YOLO, batch, conf:float=0.25) -> list[np.ndarray]:
     preds = model.model(batch["img"])
@@ -37,17 +46,20 @@ def pred_once(model:YOLO, batch, conf:float=0.25) -> list[np.ndarray]:
 
 
 def compare_visualization(
-    detector:Path, pretrained_patch:Path, 
-    data:Path, project:Path, name:str, 
-    attacker:Path=DEFAULT_ATTACKER_CFG_FILE, 
+    detector:Path, pretrained_patch:Path, save_dir:Path,
+    data:Path, attacker:Path=DEFAULT_ATTACKER_CFG_FILE, 
     batch:int=16, device:str='0',conf:float=0.25,
-    seed:int=89112, deterministic:bool=True, 
+    seed:int=89112, deterministic:bool=True, imgsz:int=512,
     **pr_arg
 )->None:
     
     init_seeds(seed=seed, deterministic=deterministic)
-    save_dir = project/name/"images"
-    save_dir.mkdir(parents=True, exist_ok=True)
+
+    save_dir = save_dir/"images"
+    save_dir_tree = {k:make_img_np_dir(save_dir/k) for k in ["clean", "attacked"]}
+    save_dir_tree['cmp'] = save_dir/"cmp"
+    save_dir_tree['cmp'].mkdir(parents=True, exist_ok=True)
+
 
     model = YOLO(detector)
     model = model.to(select_device(device=device, verbose=False))
@@ -55,7 +67,7 @@ def compare_visualization(
     advp_attacker = PatchAttacker(**load_yaml(attacker))
     
     data_args, dataset_args = get_data_args(
-        model_args=model.overrides, 
+        model_args={**(model.overrides),**{'imgsz':imgsz}}, 
         stride=max(int(model.model.stride.max()), 32),
         dataset_cfgfile_path = data,
         batch=batch,
@@ -92,31 +104,33 @@ def compare_visualization(
         )
         attack_batch = advp_attacker.attack_yolo_batch(batch=batch, patch=patch, plot=False, **pr_arg)
         dirty_arr = attack_batch["img"].detach().permute(0, 2, 3, 1).cpu().numpy()
-        dirty_img = tensor2img(attack_batch["img"])
+        dirty_imgs = tensor2img(attack_batch["img"])
         dirty_pred = pred_once(model=model, batch=attack_batch, conf=conf)
         
         dirty_pred_imgs = ultralytics_yolobatch_det_draw_boxes(
             batch=batch, preds=dirty_pred, labels=model.names, 
             need_scale=True, return_img=True, wanted_cls=[0]
         )
-        clean_arr_dir = save_dir/"clean_arr"
-        dirty_arr_dir = save_dir/"dirty_arr"
-        clean_arr_dir.mkdir(parents=True, exist_ok=True)
-        dirty_arr_dir.mkdir(parents=True, exist_ok=True)
+
 
         for idx, name in enumerate(batch["im_file"]):
     
             clean_pred_person = ((clean_preds[idx][:, -1].astype(np.int32)) == 0).astype(np.int32).sum()
             dirty_pred_person = ((dirty_pred[idx][:, -1].astype(np.int32)) == 0).astype(np.int32).sum()
     
-            canvas(
-                imlist=[clean_imgs[idx], dirty_img[idx], clean_pred_imgs[idx], dirty_pred_imgs[idx]], 
-                save_to=save_dir/Path(name).name, 
-                need_return=False
+            cv2.imwrite(
+                save_dir_tree['cmp']/Path(name).name,
+                canvas(
+                    imlist=[clean_imgs[idx], clean_pred_imgs[idx], dirty_imgs[idx], dirty_pred_imgs[idx]],  
+                )
             )
-            np.save(clean_arr_dir/Path(name).stem, clean_arr[idx])
-            np.save(dirty_arr_dir/Path(name).stem, dirty_arr[idx])
-    
+            its_path = Path(name)
+            cv2.imwrite(save_dir_tree['clean'][0]/its_path.name, clean_imgs[idx])
+            np.save(save_dir_tree['clean'][1]/its_path.stem, clean_arr[idx])
+
+            cv2.imwrite(save_dir_tree['attacked'][0]/its_path.name, dirty_imgs[idx])
+            np.save(save_dir_tree['attacked'][1]/its_path.stem,  dirty_arr[idx])
+        
             if dirty_pred_person < clean_pred_person:
                 log['less'].append({
                     'name':str(Path(name).name),
@@ -129,8 +143,10 @@ def compare_visualization(
                     'clean':int(clean_pred_person),
                     'dirty':int(dirty_pred_person)
                 })
+    
     for k in log:
         print(f"{k}: {len(log[k])}/{len(loader.dataset)}")
+    
     write_json(log, save_dir/f"cmp_person.json")
         
     
