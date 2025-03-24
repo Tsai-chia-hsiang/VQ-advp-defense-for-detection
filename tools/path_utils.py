95daedf624a72f21cb51a7116b685fa80399598d
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2025-03-25 05:01:20 +0800

    adding a arg direct_remove, if set, remove the dir without stdin required

--- tools/path_utils.py
+++ tools/path_utils.py
@@ -1,12 +1,13 @@
 import shutil
 from pathlib import Path
 
-def refresh_dir(p:Path) -> Path:
+def refresh_dir(p:Path, direct_remove:bool=False) -> Path:
     if p.is_dir():
-        msg = f"Path `{p}` already exist, If you don't want to overwrite it, stop the program (Ctrl+C), passing another directory and then run it again"
-        print(msg)
-        msg = "otherwise, it will just overwrite it"
-        _ = input(msg)
+        if not direct_remove:
+            msg = f"Path `{p}` already exist, If you don't want to overwrite it, stop the program (Ctrl+C), passing another directory and then run it again"
+            print(msg)
+            msg = "otherwise, it will just overwrite it"
+            _ = input(msg)
         shutil.rmtree(p)
     p.mkdir(parents=True)
     return p
