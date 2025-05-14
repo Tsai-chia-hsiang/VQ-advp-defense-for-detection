8ad0f07faa6518e2c028f492cef415c9b392c8e2
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2025-05-15 04:37:39 +0800

    remove the redundant functions, just leave the inference related functions

--- ultralytics_advpattack_lib/inference.py
+++ ultralytics_advpattack_lib/inference.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 import os.path as osp
+import shutil
 from ultralytics.utils import DEFAULT_CFG
 from ultralytics import YOLO
 from ultralytics.utils import ops
@@ -16,10 +17,9 @@ from pathlib import Path
 from ultralytics.utils.torch_utils import select_device, init_seeds
 from .import _LOCAL_DIR_
 sys.path.append(osp.abspath(_LOCAL_DIR_.parent))
-from tools import load_yaml, write_json
-from deepcvext.draw import canvas
-from deepcvext import tensor2img
-import gc
+from tools import load_yaml
+from deepcvext import tensor2img, img2tensor
+
 
 def make_img_np_dir(root:Path)->tuple[Path, Path]:
     arr = root/"arr"
@@ -44,144 +44,63 @@ def pred_once(model:YOLO, batch, conf:float=0.25) -> list[np.ndarray]:
         # [x,y,x,y,conf,cls]
         result[i] = result[i].cpu().numpy()
     return result
-
-
-def compare_visualization(
-    detector:Path, pretrained_patch:Path, save_dir:Path,
-    data:Path, attacker:Path=DEFAULT_ATTACKER_CFG_FILE, 
-    batch:int=16, device:str='0',conf:float=0.25,
-    seed:int=89112, deterministic:bool=True, imgsz:int=512,
-    **pr_arg
-)->None:
-    
-    init_seeds(seed=seed, deterministic=deterministic)
-
-    save_dir = save_dir/"images"
-    save_dir_tree = {k:make_img_np_dir(save_dir/k) for k in ["clean", "attacked"]}
-    save_dir_tree['cmp'] = save_dir/"cmp"
-    save_dir_tree['cmp'].mkdir(parents=True, exist_ok=True)
-
-
-    model = YOLO(detector)
-    model = model.to(select_device(device=device, verbose=False))
-    patch = torch.load(pretrained_patch, weights_only=False, map_location='cpu')['patch'].to(device=model.device)
-    advp_attacker = PatchAttacker(**load_yaml(attacker))
-    
-    data_args, dataset_args = get_data_args(
-        model_args={**(model.overrides),**{'imgsz':imgsz}}, 
-        stride=max(int(model.model.stride.max()), 32),
-        dataset_cfgfile_path = data,
-        batch=batch,
-        mode='val'
-    )
-    loader = build_dataloader(
-        dataset = build_yolo_dataset(
-            cfg = data_args,
-            img_path=dataset_args.get('val'),
-            batch=data_args.batch, 
-            data=dataset_args, 
-            mode=data_args.mode, 
-            stride = data_args.stride,
-            rect=False
-        ),
-        batch=data_args.batch, shuffle=False, 
-        workers=1
-    )
-    log = {'less':[],'redundant':[]}
-
-    for batch in tqdm(loader):
-
-        batch["img"] = batch["img"].to(dtype=torch.float32, device=model.device)/255.0
-        for k in ["batch_idx", "cls", "bboxes"]:
-            batch[k] = batch[k].to(model.device)
-        
-        clean_imgs = tensor2img(batch["img"])
-        clean_arr = batch["img"].detach().permute(0, 2, 3, 1).cpu().numpy()
-        clean_preds = pred_once(model=model, batch=batch, conf=conf)
-        
-        clean_pred_imgs = ultralytics_yolobatch_det_draw_boxes(
-            batch=batch, preds=clean_preds, labels=model.names, 
-            need_scale=True, return_img=True, wanted_cls=[0]
-        )
-        attack_batch = advp_attacker.attack_yolo_batch(batch=batch, patch=patch, plot=False, **pr_arg)
-        dirty_arr = attack_batch["img"].detach().permute(0, 2, 3, 1).cpu().numpy()
-        dirty_imgs = tensor2img(attack_batch["img"])
-        dirty_pred = pred_once(model=model, batch=attack_batch, conf=conf)
-        
-        dirty_pred_imgs = ultralytics_yolobatch_det_draw_boxes(
-            batch=batch, preds=dirty_pred, labels=model.names, 
-            need_scale=True, return_img=True, wanted_cls=[0]
-        )
-
-
-        for idx, name in enumerate(batch["im_file"]):
-    
-            clean_pred_person = ((clean_preds[idx][:, -1].astype(np.int32)) == 0).astype(np.int32).sum()
-            dirty_pred_person = ((dirty_pred[idx][:, -1].astype(np.int32)) == 0).astype(np.int32).sum()
-    
-            cv2.imwrite(
-                save_dir_tree['cmp']/Path(name).name,
-                canvas(
-                    imlist=[clean_imgs[idx], clean_pred_imgs[idx], dirty_imgs[idx], dirty_pred_imgs[idx]],  
-                )
-            )
-            its_path = Path(name)
-            cv2.imwrite(save_dir_tree['clean'][0]/its_path.name, clean_imgs[idx])
-            np.save(save_dir_tree['clean'][1]/its_path.stem, clean_arr[idx])
-
-            cv2.imwrite(save_dir_tree['attacked'][0]/its_path.name, dirty_imgs[idx])
-            np.save(save_dir_tree['attacked'][1]/its_path.stem,  dirty_arr[idx])
-        
-            if dirty_pred_person < clean_pred_person:
-                log['less'].append({
-                    'name':str(Path(name).name),
-                    'clean':int(clean_pred_person),
-                    'dirty':int(dirty_pred_person)
-                })
-            elif dirty_pred_person> clean_pred_person:
-                log['redundant'].append({
-                    'name':str(Path(name).name),
-                    'clean':int(clean_pred_person),
-                    'dirty':int(dirty_pred_person)
-                })
-    
-    for k in log:
-        print(f"{k}: {len(log[k])}/{len(loader.dataset)}")
-    
-    write_json(log, save_dir/f"cmp_person.json")
-        
-
+      
 def attack(
-    detector:Path, pretrained_patch:Path, save_dir:Path,
-    data:Path, attacker:Path=DEFAULT_ATTACKER_CFG_FILE, 
-    batch:int=16, device:str='0',conf:float=0.25,
-    seed:int=89112, deterministic:bool=True, imgsz:int=512,
-    **pr_arg
+    pretrained_patch:Path, save_dir:Path, 
+    data:Path, split:str='val',
+    attacker:Path=DEFAULT_ATTACKER_CFG_FILE, device:str='0',
+    patch_transform_args:dict=None
 )->None:   
-        init_seeds(seed=seed, deterministic=deterministic)
-        patch = torch.load(pretrained_patch, weights_only=False, map_location='cpu')['patch'].to(device=model.device)
-        advp_attacker = PatchAttacker(**load_yaml(attacker))
-        model = YOLO(detector)
-        data_args, dataset_args = get_data_args(
-            model_args={**(model.overrides),**{'imgsz':imgsz}}, 
-            stride=max(int(model.model.stride.max()), 32),
-            dataset_cfgfile_path = data,
-            batch=batch,
-            mode='val'
-        )
-        del model
-        gc.collect()
+    dev = select_device(device=device)
+    patch = torch.load(pretrained_patch, weights_only=False, map_location=dev)['patch']
+    advp_attacker = PatchAttacker(**load_yaml(attacker))
+    data_cfg:dict = load_yaml(data)
+    dataroot = Path(data_cfg['path'])/f"{data_cfg[split]}"
+    imgs_dir = dataroot/"images"
+    
+    labels_dir = dataroot/"labels"
+    assert imgs_dir.is_dir(), FileNotFoundError(f"No {imgs_dir}")
+    assert labels_dir.is_dir(), FileNotFoundError(f"No {labels_dir}")
+    
+    img_save_dir = save_dir/split/"images"
+    label_save_dir = save_dir/split/"labels"
+    assert img_save_dir != imgs_dir
+    mask_save_dir = save_dir/f"{split}_mask"
 
-        loader = build_dataloader(
-            dataset = build_yolo_dataset(
-                cfg = data_args,
-                img_path=dataset_args.get('val'),
-                batch=data_args.batch, 
-                data=dataset_args, 
-                mode=data_args.mode, 
-                stride = data_args.stride,
-                rect=False
-            ),
-            batch=data_args.batch, shuffle=False, 
-            workers=1
+    label_save_dir.mkdir(parents=True, exist_ok=True)
+    img_save_dir.mkdir(parents=True, exist_ok=True)
+    mask_save_dir.mkdir(parents=True, exist_ok=True)
+
+    img_paths:list[Path] = [_ for _ in imgs_dir.iterdir()]
+   
+    for i in tqdm(img_paths):
+        shutil.copy(labels_dir/f"{i.stem}.txt", label_save_dir/f"{i.stem}.txt")
+        label:np.ndarray = np.loadtxt(labels_dir/f"{i.stem}.txt")
+        if len(label) == 0:
+            shutil.copy(i, img_save_dir/i.name)
+            continue
+        if label.ndim < 2:
+            label = np.expand_dims(label, axis=0)
+        
+        
+        img = img2tensor(cv2.imread(i)).to(device=dev)
+        label = torch.from_numpy(label[:, 1:]).to(device=dev)
+        
+        img, masks = advp_attacker(
+            img=img, patch=patch, bboxes=label, 
+            batch_idx=torch.zeros(len(label)),
+            **patch_transform_args
         )
+    
+        cv2.imwrite(img_save_dir/i.name, img=tensor2img(img))
+        masks = tensor2img(masks)
+        m_save = mask_save_dir/i.stem
+        m_save.mkdir(parents=True, exist_ok=True)
+        if isinstance(masks, np.ndarray):
+            cv2.imwrite(m_save/f"0.png", img=masks)
+        elif isinstance(masks, list):
+            for bi, mi in enumerate(masks):
+                cv2.imwrite(m_save/f"{bi}.png", img=mi)
+        else:
+            raise TypeError(f"{type(masks)} is not a list of ndarray or ndarray")
+
