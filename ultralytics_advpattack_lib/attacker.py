4c6b95273e27bfdbb9af621dfa8bc0addcbf0dc5
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2025-05-15 04:36:56 +0800

    adding returning mask

--- ultralytics_advpattack_lib/attacker.py
+++ ultralytics_advpattack_lib/attacker.py
@@ -63,16 +63,16 @@ class PatchAttacker():
         
         theta[:, 0, 0] = cos/scale
         theta[:, 0, 1] = sin/scale
-        theta[:, 0, 2] = (tx*cos + ty*sin)/scale
+        theta[:, 0, 2] = tx*cos/scale + ty*sin/scale
         
         theta[:, 1, 0] = -sin/scale
         theta[:, 1, 1] = cos/scale
-        theta[:, 1, 2] = (-tx*sin + ty*cos)/scale
+        theta[:, 1, 2] = -tx*sin/scale + ty*cos/scale
         
         grid = F.affine_grid(theta, patch.size(), align_corners=False)
         return F.grid_sample(patch, grid, align_corners=False)
         
-    def __call__(self, img:torch.Tensor, patch:torch.Tensor, bboxes:torch.Tensor, batch_idx:torch.Tensor, patch_random_rotate:bool=True, patch_blur:bool=True):
+    def __call__(self, img:torch.Tensor, patch:torch.Tensor, bboxes:torch.Tensor, batch_idx:torch.Tensor, return_mask:bool=True, patch_random_rotate:bool=True, patch_blur:bool=True):
         
         assert img.device == patch.device
         dev = img.device
@@ -104,7 +104,7 @@ class PatchAttacker():
         patch = self.patch_affine_transformation(patch=patch, xywh=bboxes, angle=angle, scale=geo_scale)
 
         # for each img, attacking it independently
-        masks = torch.zeros_like(img).to(torch.int32)
+        
         for i in range(img.size(0)):
             bidx = torch.where(batch_idx == i)[0]
             using_patch = patch[bidx]
@@ -112,11 +112,12 @@ class PatchAttacker():
             for obj_adv_patch in using_patch:
                 mask_location = (obj_adv_patch == 0)
                 img[i] = torch.where(mask_location, img[i], obj_adv_patch)
-                masks[i] = torch.where(mask_location, 0, 1)
+        if return_mask:
+            masks = torch.where(patch > 0, 1, 0).to(torch.int32).sum(dim=1, keepdim=True).clamp_(0, 1)
+            return img, masks
+        return img, None
 
-        return img, masks
-
-    def attack_yolo_batch(self, batch, patch:torch.Tensor, patch_random_rotate:bool=False, patch_blur:bool=True, plot:bool=False, mask:bool=True,**kwargs) -> dict:
+    def attack_yolo_batch(self, batch, patch:torch.Tensor, patch_random_rotate:bool=False, patch_blur:bool=True, plot:bool=False, mask:bool=False,**kwargs) -> dict:
         """
         Apply the transformation in place at the batch level, 
         
@@ -132,7 +133,7 @@ class PatchAttacker():
         - random_rotation_patch: bool, default False
             - Wether to apply random ratation
                 - For attacker's argument
-        - mask: bool, default True
+        - mask: bool, default False
             - Wether return masks of attacked location 
         - plot: bool, defualt False
             - Wether plotting the results of images under attack..
@@ -148,10 +149,11 @@ class PatchAttacker():
             patch = patch, img=batch["img"],  
             bboxes=batch['bboxes'], 
             batch_idx=batch['batch_idx'], 
+            return_mask = mask,
             patch_random_rotate=patch_random_rotate,
             patch_blur=patch_blur
         )
-        if mask:
+        if masks is not None:
             batch['advp_mask'] = masks
 
         if plot:
