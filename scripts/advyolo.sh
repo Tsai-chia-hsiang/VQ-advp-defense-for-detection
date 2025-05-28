2023d4affc1829202aeba7e79afc7210585c84d1
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2025-05-28 11:54:18 +0800

    del submoudle config

--- scripts/advyolo.sh
+++
@@ -1,8 +0,0 @@
-name=advyolo 
-LR=0.03
-conf=0.25
-# --sup_prob_loss --logit_to_prob
-python advpatch_attack.py \
-    --task $1 --name $name \
-    --patch_random_rotate --patch_blur \
-    --conf $conf  --lr $LR  --vq
